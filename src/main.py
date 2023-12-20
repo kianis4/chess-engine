f17e47583ff4a03e7abdb896ad1b43c39466dc1c
Suleyman <suley.kiani@gmail.com>
2023-12-20 18:57:53 -0500

    Piece dragging over background fix

--- src/main.py
+++ src/main.py
@@ -44,7 +44,10 @@ class Main:
                 elif event.type == pygame.MOUSEMOTION:
                     if dragger.dragging:
                         dragger.update_mouse(event.pos)
+                        game.show_bg(screen)
+                        game.show_pieces(screen)
                         dragger.update_blit(screen)
+
 
 
 
