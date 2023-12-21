5e65c221a48d7e1aa8c7f39d3ffd713464f5c94e
Suleyman <suley.kiani@gmail.com>
2023-12-20 20:20:05 -0500

    Add Color class in color.py
    Introduce a Color class representing a pair of light and dark colors. This class is designed to facilitate handling color schemes in graphical elements within the application.
    
    Parameters:
    - : Light color.
    - : Dark color.

---
+++ src/color.py
@@ -0,0 +1,13 @@
+# color.py
+
+class Color:
+    def __init__(self, light, dark):
+        """
+        Color class to represent a pair of light and dark colors.
+
+        Parameters:
+        - light: Light color.
+        - dark: Dark color.
+        """
+        self.light = light
+        self.dark = dark
